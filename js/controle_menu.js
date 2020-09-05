e4d8a7f4bb5d241295ad7a44ba5eb8ba294a336d
Igor Gavilon <igorgavilon@gmail.com>
2020-09-05 16:02:09 -0400

    Add files via upload

---
+++ js/controle_menu.js
@@ -0,0 +1,7 @@
+function fecharMenu(){
+    document.getElementsByClassName("menu")[0].style.display = "none";
+}
+function abrirMenu(){
+    document.getElementsByClassName("menu")[0].style.display = "inline";
+
+}
